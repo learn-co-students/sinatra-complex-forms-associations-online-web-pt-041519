5b925015b08e52d1f576717e24a3d3403a813c5e
Renee Nordholm <reneenordholm@gmail.com>
2019-08-02 13:34:55 -0500

    Done.

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,29 +1,57 @@
 class OwnersController < ApplicationController
 
+  # index
   get '/owners' do
     @owners = Owner.all
+     
     erb :'/owners/index' 
   end
 
+  # create
   get '/owners/new' do 
+    @pets = Pet.all
+
     erb :'/owners/new'
   end
 
-  post '/owners' do 
-    
+  # create
+  post '/owners' do
+    @owner = Owner.create(params[:owner])
+    if !params[:pet][:name].empty?
+      @owner.pets << Pet.create(name: params[:pet][:name])
+    end
+
+    redirect "/owners/#{@owner.id}"
   end
 
+  # update
   get '/owners/:id/edit' do 
+    @pets = Pet.all
     @owner = Owner.find(params[:id])
+
     erb :'/owners/edit'
   end
 
-  get '/owners/:id' do 
+    # read
+    get '/owners/:id' do 
+      @owner = Owner.find(params[:id])
+    
+      erb :'/owners/show'
+    end
+
+  # update
+  patch '/owners/:id' do 
+    if !params[:owner].keys.include?("pet_ids")
+      params[:owner][:pet_ids] = []
+    end
+
     @owner = Owner.find(params[:id])
-    erb :'/owners/show'
+    @owner.update(params[:owner])
+      if !params[:pet][:name].empty?
+        @owner.pets << Pet.create(name: params[:pet][:name])
+      end
+    redirect "owners/#{@owner.id}"
   end
 
-  patch '/owners/:id' do 
-   
-  end
+
 end
--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,26 +1,60 @@
 class PetsController < ApplicationController
 
+  # index
   get '/pets' do
     @pets = Pet.all
+
     erb :'/pets/index' 
   end
 
+  # create
   get '/pets/new' do 
+    @owners = Owner.all
+
     erb :'/pets/new'
   end
 
+  # create
   post '/pets' do 
+    @pet = Pet.create(params[:pet])
+    @pet.update(owner_id: params[:owner_id]) if params[:owner_id]
+    
+    if !params[:owner][:name].empty?
+      @pet.owner = Owner.create(name: params[:owner][:name])
+      @pet.save
+    end
 
     redirect to "pets/#{@pet.id}"
   end
 
+  # read
   get '/pets/:id' do 
     @pet = Pet.find(params[:id])
+
     erb :'/pets/show'
   end
 
+  # update
+  get '/pets/:id/edit' do 
+    @owners = Owner.all
+    @pet = Pet.find(params[:id])
+  
+    erb :'/pets/edit'
+  end
+
+  # update
   patch '/pets/:id' do 
+    @pet = Pet.find(params[:id])
+    @pet.owner_id = params[:owner_id] if params[:owner_id]
+
+      if !params[:owner][:name].empty?
+        @pet.owner = Owner.create(name: params[:owner][:name])
+        @pet.save
+      end
+
+    @pet.update(params[:pet])
 
     redirect to "pets/#{@pet.id}"
   end
+  
 end
